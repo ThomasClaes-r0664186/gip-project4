a2fbd7473fcb3eb3a833ebb9dd58cbf572e55132
Thomas Claes <r0664186@ucll.be>
2020-11-17 17:55:16 +0100

    It's a whole new world

---
+++ src/main/java/be/ucll/GipEsportsManagerApplication.java
@@ -0,0 +1,13 @@
+package be.ucll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GipEsportsManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GipEsportsManagerApplication.class, args);
+	}
+
+}
